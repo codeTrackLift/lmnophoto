9a1027ba291effa4decfb2e8fb83754bf5d6cf8a
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-01-25 11:16:45 -0500

    Scroll Magic Scenes
    Added one scene for each photo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function cameraSound() {
 var controller1 = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
     triggerElement: '.scene1',
-    offset: 100,
+    offset: -100,
     duration: 400
 })
 .setClassToggle('.scene1', 'show')
@@ -26,8 +26,71 @@ var scene2 = new ScrollMagic.Scene({
 var controller3 = new ScrollMagic.Controller();
 var scene3 = new ScrollMagic.Scene({
     triggerElement: '.scene3',
-    offset: 100,
+    offset: -100,
     duration: 400
 })
 .setClassToggle('.scene3', 'show')
 .addTo(controller3);
+
+var controller4 = new ScrollMagic.Controller();
+var scene4 = new ScrollMagic.Scene({
+    triggerElement: '.scene4',
+    offset: 100,
+    duration: 400
+})
+.setClassToggle('.scene4', 'show')
+.addTo(controller4);
+
+var controller5 = new ScrollMagic.Controller();
+var scene5 = new ScrollMagic.Scene({
+    triggerElement: '.scene5',
+    offset: -100,
+    duration: 400
+})
+.setClassToggle('.scene5', 'show')
+.addTo(controller5);
+
+var controller6 = new ScrollMagic.Controller();
+var scene6 = new ScrollMagic.Scene({
+    triggerElement: '.scene6',
+    offset: 100,
+    duration: 400
+})
+.setClassToggle('.scene6', 'show')
+.addTo(controller6);
+
+var controller7 = new ScrollMagic.Controller();
+var scene7 = new ScrollMagic.Scene({
+    triggerElement: '.scene7',
+    offset: -100,
+    duration: 400
+})
+.setClassToggle('.scene7', 'show')
+.addTo(controller7);
+
+var controller8 = new ScrollMagic.Controller();
+var scene8 = new ScrollMagic.Scene({
+    triggerElement: '.scene8',
+    offset: -100,
+    duration: 400
+})
+.setClassToggle('.scene8', 'show')
+.addTo(controller8);
+
+var controller9 = new ScrollMagic.Controller();
+var scene9 = new ScrollMagic.Scene({
+    triggerElement: '.scene9',
+    offset: 100,
+    duration: 400
+})
+.setClassToggle('.scene9', 'show')
+.addTo(controller9);
+
+var controller10 = new ScrollMagic.Controller();
+var scene10 = new ScrollMagic.Scene({
+    triggerElement: '.scene10',
+    offset: -100,
+    duration: 400
+})
+.setClassToggle('.scene10', 'show')
+.addTo(controller10);
