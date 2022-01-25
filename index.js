14494e6de617684fa64e376d8266b9cbd9edfa55
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-01-25 11:19:50 -0500

    Scroll Magic Duration
    Alternated scroll magic duration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var controller1 = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
     triggerElement: '.scene1',
     offset: -100,
-    duration: 400
+    duration: 500
 })
 .setClassToggle('.scene1', 'show')
 .addTo(controller1);
@@ -18,7 +18,7 @@ var controller2 = new ScrollMagic.Controller();
 var scene2 = new ScrollMagic.Scene({
     triggerElement: '.scene2',
     offset: 100,
-    duration: 400
+    duration: 600
 })
 .setClassToggle('.scene2', 'show')
 .addTo(controller2);
@@ -27,7 +27,7 @@ var controller3 = new ScrollMagic.Controller();
 var scene3 = new ScrollMagic.Scene({
     triggerElement: '.scene3',
     offset: -100,
-    duration: 400
+    duration: 500
 })
 .setClassToggle('.scene3', 'show')
 .addTo(controller3);
@@ -36,7 +36,7 @@ var controller4 = new ScrollMagic.Controller();
 var scene4 = new ScrollMagic.Scene({
     triggerElement: '.scene4',
     offset: 100,
-    duration: 400
+    duration: 600
 })
 .setClassToggle('.scene4', 'show')
 .addTo(controller4);
@@ -45,7 +45,7 @@ var controller5 = new ScrollMagic.Controller();
 var scene5 = new ScrollMagic.Scene({
     triggerElement: '.scene5',
     offset: -100,
-    duration: 400
+    duration: 500
 })
 .setClassToggle('.scene5', 'show')
 .addTo(controller5);
@@ -54,7 +54,7 @@ var controller6 = new ScrollMagic.Controller();
 var scene6 = new ScrollMagic.Scene({
     triggerElement: '.scene6',
     offset: 100,
-    duration: 400
+    duration: 600
 })
 .setClassToggle('.scene6', 'show')
 .addTo(controller6);
@@ -63,7 +63,7 @@ var controller7 = new ScrollMagic.Controller();
 var scene7 = new ScrollMagic.Scene({
     triggerElement: '.scene7',
     offset: -100,
-    duration: 400
+    duration: 500
 })
 .setClassToggle('.scene7', 'show')
 .addTo(controller7);
@@ -72,7 +72,7 @@ var controller8 = new ScrollMagic.Controller();
 var scene8 = new ScrollMagic.Scene({
     triggerElement: '.scene8',
     offset: -100,
-    duration: 400
+    duration: 600
 })
 .setClassToggle('.scene8', 'show')
 .addTo(controller8);
@@ -81,7 +81,7 @@ var controller9 = new ScrollMagic.Controller();
 var scene9 = new ScrollMagic.Scene({
     triggerElement: '.scene9',
     offset: 100,
-    duration: 400
+    duration: 500
 })
 .setClassToggle('.scene9', 'show')
 .addTo(controller9);
@@ -90,7 +90,7 @@ var controller10 = new ScrollMagic.Controller();
 var scene10 = new ScrollMagic.Scene({
     triggerElement: '.scene10',
     offset: -100,
-    duration: 400
+    duration: 600
 })
 .setClassToggle('.scene10', 'show')
 .addTo(controller10);
